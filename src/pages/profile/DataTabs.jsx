e929921164ae6ed856a5a3ea9e4a03b4a9818bd6
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-05-07 23:53:01 +0530

    feat: add profile page

---
+++ src/pages/profile/DataTabs.jsx
@@ -0,0 +1,20 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+
+export function DataTabs({ data }) {
+    return (
+      <Tabs>
+        <TabList>
+          {data.map((tab, index) => (
+            <Tab key={index}>{tab.label}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {data.map((tab, index) => (
+            <TabPanel p={4} key={index}>
+              {tab.content}
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    )
+}
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,37 @@
+import { Avatar, Flex, Text, IconButton } from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons'
+import {DataTabs} from "./DataTabs";
+
+const tabData = [
+    {
+      label: 'Nigerian Jollof',
+      content: 'Perhaps the greatest dish ever invented.',
+    },
+    {
+      label: 'Pounded Yam & Egusi',
+      content:
+        'Perhaps the surest dish ever invented but fills the stomach more than rice.',
+    },
+]
+
 export function Profile(){
     return (
-        <h1>profile</h1>
+        <>
+            <Flex justify="center" align="center" mt="2rem" gap={10}>
+                <Avatar size='2xl' name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+                <Flex direction="column">
+                    <Text fontSize="3xl">Aniet Prksf</Text>
+                    <Text fontSize="xl">test@test.com</Text>
+                </Flex>
+                <IconButton variant="outline" aria-label='Search database' icon={<EditIcon />} />
+            </Flex>
+            <Flex justify="center" align="center" mt={2} gap={10}>
+                <Text fontSize="xl">POD - D</Text>
+                <Text fontSize="xl">TEAM - D3</Text>
+            </Flex>
+
+            <DataTabs data={tabData} />
+            
+        </>
     );
 }
